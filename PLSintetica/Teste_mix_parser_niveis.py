e5b7bff0b95206d24c2c4772c3a1fd9cbd57fc6f
Lucas Lage <lucasflage@hotmail.com>
2019-07-01 21:03:18 -0300

    alteracoes,testes obj PL

--- PLSintetica/Teste_mix_parser_niveis.py
+++ PLSintetica/Teste_mix_parser_niveis.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 13 16:19:31 2019
 
-@author: lucas
-"""
-
-# -*- coding: utf-8 -*-
 """
 Created on Tue May 14 13:52:27 2019
 
@@ -14,27 +8,28 @@ Created on Tue May 14 13:52:27 2019
 import re
 
 def nextLevelSearch(strInput):
-    indexList = []
-    notNoneList = []
-    dic = {
-    "ArtSearch" : re.search(r'(\nArt\. \dº|\nArt\. \d+)',strInput),        
-    "ParSearch" : re.search(r'§ \d',strInput),
-    "AliSearch" : re.search(r'(\n[a-z]\)|\n[a-z][a-z]\))',strInput),
-    "IncSearch" : re.search(r'[MDCLXVI]+\ -',strInput),
-    }
+    indexList = []    
+    tup = (
+    ("ArtSearch" , re.search(r'\nArt\. \dº|\nArt\. \d+',strInput)),        
+    ("ParSearch" , re.search(r'§ \d',strInput)),
+    ("AliSearch" , re.search(r'\n[a-z]\)|\n[a-z][a-z]\)',strInput)),
+    ("IncSearch" , re.search(r'[MDCLXVI]+\ -',strInput)),
+    )
     
-    for key,value in dic:
+    for key,value in tup:
         try:
             index = value.start()
-            notNoneList.append(value)
             indexList.append(index)
         except:
             pass
-        
-    for key,value in dic:
-        if min(indexList) == value.start():
-            return (key,value) 
-    
+
+    for key,value in tup:
+        try:            
+            if min(indexList) == value.start():
+                return (value) 
+        except:
+            pass
+    return('')
 
 
 IndexList = []
@@ -44,37 +39,70 @@ PlPar = []
 PlInc = []
 PlAli = []
 
-artPat = re.compile(r'(\nArt\. \dº|\nArt\. \d+)')
-parPat = re.compile(r'§ \d')
-aliPat = re.compile(r'(\n[a-z]\)|\n[a-z][a-z]\))')
-incPat = re.compile(r'[MDCLXVI]+\ -')
+artPat = re.compile(r'\nArt\. \dº. +|\nArt\. \d+. +')
+parPat = re.compile(r'\n§ \d+º|\nParágrafo único\.')
+incPat = re.compile(r'\n[MDCLXVI]+\ -')
+aliPat = re.compile(r'\n[a-z]\)|\n[a-z][a-z]\)')
+
+pars = []
 
 with open ('teste/MPV870_txt_site.txt', 'r', encoding = 'utf-8') as pl:
     texto = pl.read()
+        
+    arts = re.split(artPat,texto)
+    for art in arts:
+        if arts.index(art) == 0:
+            PlObj.append(art)
+# =============================================================================
+#         else:
+#             PlArt.append(re.match(r'.+\b',art.strip())[0])
+# =============================================================================
+                      
+        
+        paragraphs = re.split(parPat,art)   
+        pars.append(paragraphs)         
+        for paragraph in paragraphs:
+            if paragraphs.index(paragraph) == 0:
+                PlPar.append(re.match(r'.+\b',art.strip())[0])
+            else:
+                PlPar.append(re.match(r'.+\b',paragraph.strip())[0])
+                
+            incisos = re.split(incPat,paragraph)
+            for inciso in incisos:
+                if incisos.index(inciso) == 0:
+                    continue
+                else:
+                    PlInc.append(re.match(r'.+\b',inciso.strip())[0])
+                    
+                    
+                
+                alineas = re.split(aliPat,inciso)
+                if len(alineas) > 1:
+                    for alinea in alineas:
+                        PlAli.append(re.match(r'.+\b',alinea.strip())[0])
+                        
+                if PlAli:
+                    PlInc.append(PlAli)
+                PlAli=[]
+            
+            if PlInc:
+                PlPar.append(PlInc)
+            PlInc = []
+        if PlPar:    
+            PlArt.append(PlPar)
+        PlPar = []
+        
+    PlObj.append(PlArt)
 
-    while texto:
-        
-        ArtSearch = re.search(r'\nArt\. \dº|\nArt\. \d\d',texto)
-        ParSearch = re.search(r'§ \d',texto)
-        AliSearch = re.search(r'\n[a-z]\)|\n[a-z][a-z]\)',texto)
-        IncSearch = re.search(r'[MDCLXVI]+\ -',texto)
-        
-        arts = re.split(r'(\nArt\. \dº|\nArt\. \d+)',texto)
-        for art in arts:
-            
-            ParSearch = re.search(parPat,texto)
-            AliSearch = re.search(aliPat,texto)
-            
-            PlArt.append()
-            
-            paragraphs = re.split(r'§ \d',texto)
-            
-# =============================================================================
-#             for paragraph in paragraphs:
-# =============================================================================
+                        
+                    
+                    
+                    
                 
         
-
+# =============================================================================
+#         PlArt.append[art[:x.start()]] 
+# =============================================================================
 # =============================================================================
 #         if IndexList.count(None) == 1:
 #             print("acabou")
