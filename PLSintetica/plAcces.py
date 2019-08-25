b081ab19c1a923383bb6816fb1f587cd2d905840
Lucas Lage <lucasflage@hotmail.com>
2019-08-24 21:01:12 -0300

    script de acesso e modificacao do plObj

--- PLSintetica/plAcces.py
+++ PLSintetica/plAcces.py
@@ -42,6 +42,51 @@ if artigo and paragrafo and inciso and not alinea:
         
 if artigo and paragrafo and not inciso and not alinea:
     try:
-        print(mpv[1][lista_in[0]][lista_in[1]])
+        print(mpv[1][lista_in[0]][lista_in[2]])
     except:
         print("não existe")
+        
+texto_ADD = "texto adicionado"
+texto_MOD = "texto_mod"
+
+tipo_emenda = "SUP"
+
+# teste: adicionar texto ao artigo 5 paragrafo 0 inciso 1
+
+if tipo_emenda == "ADD":
+    # não é necessário considerar a posição da alínea para inserir ela
+        
+    if artigo and paragrafo and inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][1].append(texto_ADD)
+        print(mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][1])
+        
+    if artigo and paragrafo and not inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]].append(texto_ADD)
+        print(mpv[1][lista_in[0]][lista_in[1]])
+    
+    if artigo and not paragrafo and inciso and not alinea:
+        mpv[1][lista_in[0]][0][1][lista_in[2]].append(texto_ADD)
+    
+if tipo_emenda == "MOD":
+    if artigo and paragrafo and inciso and alinea:
+        mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][1][lista_in[3]] = texto_MOD
+        print(mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][1][lista_in[3]])
+        
+    if artigo and paragrafo and inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][0] = texto_MOD
+        
+    if artigo and paragrafo and not inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]][0]  = texto_MOD
+    
+if tipo_emenda == "SUP":
+    if artigo and paragrafo and inciso and alinea:
+        mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][1][lista_in[3]] = ''
+        
+    if artigo and paragrafo and inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]][1][lista_in[2]][0] = ''
+        
+    if artigo and paragrafo and not inciso and not alinea:
+        mpv[1][lista_in[0]][lista_in[1]][0]  = ''
+
+    
+    
