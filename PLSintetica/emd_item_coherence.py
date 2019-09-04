d522336529bdb2d769a720b1d10ee88bb7297a44
Lucas Lage <lucasflage@hotmail.com>
2019-09-04 14:17:22 -0300

    alterado tratamento da saída do agregador

--- PLSintetica/emd_item_coherence.py
+++ PLSintetica/emd_item_coherence.py
@@ -3,24 +3,85 @@
 
 import re
 import os
-import ClassificadorDeEmendas as clfe
-import plObjFunc as pl
+import sys
+from unidecode import unidecode
 import pandas as pd
+import nltk
 from gensim.models import KeyedVectors
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.corpus import stopwords
-from unicodedata import normalize
+import ClassificadorDeEmendas as clfe
+import plObjFunc as pl
 
 
+
+def proc_agreg_out(agr_out):
+    bet_quote = re.compile(r"\'(.*?)\'", flags = re.IGNORECASE)
+    existe_agreg = False
+    if re.search('__INICIO_AGREGADOR__',emd):
+        nxt = emd.split("__INICIO_AGREGADOR__\n")[1]
+        if re.search(r'.+\n(?=__FIM_AGREGADOR__)',nxt):
+            ent = nxt.split('__FIM_AGREGADOR__')[0] 
+            existe_agreg = True
+        
+    lista_indices = []
+    if existe_agreg:
+        for item in ent.splitlines():
+            item = re.findall(bet_quote,item)
+            pl_index = []
+            #Le todos os casos, cria uma lista com os indices para o 
+            # obj pl
+            for nr,index in enumerate(item):
+                try:
+                    if nr == 0 or nr == 1:
+                        pl_index.append(int(index.split('_')[1]))
+                    if nr == 2:
+                        pl_index.append(pl.roman_to_int(index.split('_')[1]))
+                    if nr == 3:
+                        pl_index.append(pl.ali_to_num(index.split('_')[1]))
+                except:
+                    pl_index.append(None)                        
+                        
+            # Tratamento de casos especiais
+            # Desconsidera caso não exista artigo
+            if not pl_index[0]:
+                continue
+            # Caso receba somente artigo e não parágrafo, use o 
+            # parágrafo 0
+            if pl_index[0] and not pl_index[1] and pl_index[2]:
+                pl_index[1] = 0
+            # Caso receba somente artigo e alinea 
+            if pl_index[0] and not pl_index[1] and not pl_index[3] and pl_index[3]:
+                pl_index[1] = 0
+                pl_index[2] = 1
+                
+            # Retira valores None e inserir indices para correta localização
+            pl_index = list(filter(None,pl_index))
+            if len(pl_index) == 4:
+                pl_index.insert(2,1)
+                pl_index.insert(-1,1)
+            if len(pl_index) == 3:
+                pl_index.insert(-1,1)
+                
+            lista_indices.append(pl_index)
+    return lista_indices
+        
 
 mpv = pl.PlObjCreate('./teste/mpv870/MPV870_txt_site.txt')
-
-emd_dir_path = '../parser_itens/emendas_processadas_em_blocos'
-
+emd_dir_path = '../parser_itens/emd_pars_agreg'
 modelPath = '../coherence/languagemodel/vectors_new.bin'
 
-stop_word = stopwords.words('portuguese')
-tokenizer = CountVectorizer(stop_words=stopwords.words('portuguese')).build_tokenizer()
+
+# =============================================================================
+# # Caminho para o arquivo .txt do inteiro teor     
+# mpv = pl.PlObjCreate(sys.argv[1])
+# # Caminho para a pasta contendo as emendas tratadas pelo agregador
+# emd_dir_path = sys.argv[2]
+# # Caminho para o modelo .bin treinado do Skipgram
+# modelPath = sys.argv[3]
+# =============================================================================
+
+stp_wrd = nltk.download('stopwords')
+tokenizer = CountVectorizer(stop_words=stp_wrd).build_tokenizer()
 model = KeyedVectors.load_word2vec_format(modelPath, binary=True)
 
 
@@ -41,80 +102,28 @@ for file,tipo_emenda in zip(files,tipos_de_emenda):
         lista_de_itens_alterados = []
         
         emd = emenda.read()
-        texto_emd = tokenizer(emd.split("!@#$%")[0].lower())
-        # Processa emendas pós agregador    
-        if re.search('__INICIO_AGREGADOR__',emd):
-            nxt = emd.split("__INICIO_AGREGADOR__\n")[1]
-            if re.search(r'.+\n(?=__FIM_AGREGADOR__)',nxt):
-                ent = nxt.split('__FIM_AGREGADOR__')[0] 
-        else:
-            continue
-        # cria lista com indices
-        for item in ent.splitlines():
-            indices = re.sub(r"('|\(|\))",'',item).split(',')
-            pl_index = []
-            for i in indices:
-                try:
-                    pl_index.append(i.split('_')[1])
-                except:
-                    pl_index.append(None)
-                    
-            if pl_index[0]:
-                pl_index[0] = int(pl_index[0])
-            if pl_index[1]:
-                try:
-                    pl_index[1] = int(pl_index[1])
-                except:
-                    pl_index[1] = pl.roman_to_int(pl_index[1])
-            if pl_index[2]:
-                try:
-                    pl_index[2] = pl.roman_to_int(pl_index[2])
-                except:
-                    if not pl_index[3] and pl_index[2].isalpha():
-                        pl_index[3] = pl_index[2]
-                        pl_index[2] = None
-            if pl_index[3]:
-                pl_index[3] = pl.ali_to_num(pl_index[3])   
-            lista_de_itens_alterados.append(pl_index)
-        
-        
+        texto_emd = tokenizer(unidecode(emd.split("!@#$%")[0].lower()))
+                                                  
+        # Processa emendas pós agregador
+        lista_de_itens_alterados = proc_agreg_out(emd)
+
         for alteracao in lista_de_itens_alterados:
-            try:            
-                if tipo_emenda == "MOD":
-# =============================================================================
-#                     if alteracao[0] and alteracao[1] and alteracao[2] and alteracao[3]:
-#                         texto_pl = mpv[1][alteracao[0]][alteracao[1]][1][alteracao[2]][1][alteracao[3]]
-#                         texto_pl = ' '.join(list(pl.flatten(texto_pl)))
-#                         
-#                     if alteracao[0] and alteracao[1] and alteracao[2] and not alteracao[3]:
-#                         texto_pl = mpv[1][alteracao[0]][alteracao[1]][1][alteracao[2]]
-#                         texto_pl = ' '.join(list(pl.flatten(texto_pl)))
-#                 
-#                     if alteracao[0] and alteracao[1] and not alteracao[2] and not alteracao[3]:
-#                         texto_pl = mpv[1][alteracao[0]][alteracao[1]]
-#                         texto_pl = ' '.join(list(pl.flatten(texto_pl)))
-#                     
-#                     if alteracao[0] and not alteracao[1] and not alteracao[2] and not alteracao[3]:
-#                         texto_pl = mpv[1][alteracao[0]]
-#                         texto_pl = remover_acentos(' '.join(list(pl.flatten(texto_pl))).lower())
-# =============================================================================
+            if tipo_emenda == "MOD":
                     # Procura posição no plObj
-                    texto_pl = pl.nested_lookup(mpv[1],list(filter(None, alteracao)))
-                    
+                    try:
+                        texto_pl = pl.nested_lookup(mpv[1],alteracao)
+                    except:
+                        print("Elemento não encontrado")
                     # Cria uma string inteira com todos os textos na posição encontrada
                     texto_pl = ' '.join(list(pl.flatten(texto_pl))).lower()
+             
+                    texto_pl = tokenizer(unidecode(texto_pl))
                     
-                    texto_pl = tokenizer(texto_pl)
-                    try:
-                        dist = model.distance(texto_pl,texto_emd)
-                    except:
-                        print('erro no modelo')
-                    to_df_row.append([file,' '.join(alteracao),dist])      
+                    dist = model.wmdistance(texto_pl,texto_emd)
+                    to_df_row.append([file,' '.join(str(alteracao)),dist])      
                        
-                
-                
-            except:
-                
-                pass
 df = pd.DataFrame(to_df_row)
 df.to_csv('df_csv.csv')
+
+
+
